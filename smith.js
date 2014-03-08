887e1c19e1c39da3c2143d05b5cd6e6c77345a9d
Todd Branchflower <todd.branchflower@gmail.com>
2014-03-08 09:36:14 -0700

    created alternate build script for use with metalsmith

---
+++ smith.js
@@ -0,0 +1,30 @@
+var marked = require('marked')
+    , fs = require('fs')
+    , path = require('path')
+    , ejs = require('ejs')
+    , Metalsmith = require('metalsmith');
+
+var args = process.argv;
+
+if (args.length != 5)
+  console.log("Format is <node course_path template_path course_id>");
+else
+{
+  build(args[2], args[3], args[4]);
+}
+
+function build(coursePath, templatePath, course_id)
+{
+  if (fs.existsSync(templatePath) && fs.existsSync(coursePath))
+  {
+    course_id = course_id || "";
+
+    Metalsmith(coursePath)
+              .use(marked)
+              .use(templates('ejs'))
+              .build();
+  } else
+  {
+    console.log("Either the course path or template path do not exist.");
+  }
+}
